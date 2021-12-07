a54adc0f8d37dbfeedbbf1fa09c46cce3815bfd4
Rasmus Fuhse <krassmus@gmail.com>
2021-12-07 14:08:28 +0100

    bugfix margin

--- assets/theswitcher.js
+++ assets/theswitcher.js
@@ -1,3 +1,3 @@
 $(function () {
-    $('<a title="Designs anpassen" data-dialog="size=auto" href="' + STUDIP.URLHelper.getURL('plugins.php/mycss/theswitcher/index') + '" style="margin-right: 30px;"><img src="' + STUDIP.ASSETS_URL + '/images/icons/blue/group4.svg" style="width: 28px;"></a>').appendTo('.helpbar-container');
+    $('<a title="Designs anpassen" data-dialog="size=auto" href="' + STUDIP.URLHelper.getURL('plugins.php/mycss/theswitcher/index') + '" style="margin-right: 7px;"><img src="' + STUDIP.ASSETS_URL + '/images/icons/blue/group4.svg" style="width: 28px;"></a>').appendTo('.helpbar-container');
 });
